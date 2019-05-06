e04f823edd821dbbf4ba0a9540b604aa834698a0
Oscar Perez MacOs <operez@na-at.com.mx>
2019-05-05 21:58:50 -0500

    -Removed open from base result

--- data/src/main/java/com/booleansystems/data/common/IBaseResultListener.kt
+++ data/src/main/java/com/booleansystems/data/common/IBaseResultListener.kt
@@ -6,6 +6,6 @@ import com.booleansystems.domain.common.BaseResponse
 Created by oscar on 03/05/19
 Notify is response is success or not */
 interface IBaseResultListener<T : BaseResponse<*>> {
-    open fun onSuccess(response: T);
-    open fun onError(error: Throwable)
+    fun onSuccess(response: T);
+    fun onError(error: Throwable)
 }
